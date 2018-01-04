594ca0475685067d3fc2c34e05310d3472d69351
Paul Seung Mo Lim <Paulsmlim@gmail.com>
2018-01-04 12:33:43 -0500

    Update Professor.java

--- classes/Professor.java
+++ classes/Professor.java
@@ -1,20 +1,12 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;
 
 /**
- * Write a description of class Professor here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Professor is whom Paul has to submit his code to.
+ * She is in both Classrooms
  */
 public class Professor extends Actor
 {
-    //The best teacher ever. If Paul does not have his USB, she will yell at him to get it. If Paul does have his USB,
-    //then she will give him an A+
-    /**
-     * Act - do whatever the MrsNastai wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act() 
+    public void act()
     {
         
     }    
