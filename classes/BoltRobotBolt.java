9b668fc0303edc394b9b01acb9417e8eb25880f1
Paul Seung Mo Lim <Paulsmlim@gmail.com>
2018-01-04 17:30:12 -0500

    Update BoltRobotBolt.java

--- classes/BoltRobotBolt.java
+++ classes/BoltRobotBolt.java
@@ -1,35 +1,34 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;
 
 /**
- * Write a description of class BoltRobotBolt here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Bolt of the object class BoltRobot. Each bolt that hits Paul takes
+ * away 50 health points from Paul
  */
 public class BoltRobotBolt extends Actor
 {
+    // Time counter for object image changes
     private int time = 0;
     private int imageNum = 1;
-    //True if Bolt touched Paul or world edge
+    // True if Bolt hit Paul or world edge
     private boolean splat = false;
-    /**
-     * Act - do whatever the Bolt wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act() 
+
+    public void act()
     {
         if(!splat){
             checkKeys();
+        } else {
+            checkSplat();
         }
-        checkSplat();
     }    
 
     public void checkKeys()
     {
         move(3);
         time++;
+        // Image of BoltRobotBolt changes for every 6th call of this function
         if(time%6 == 0){
             setImage("BoltRobotBolt-" + imageNum + ".png");
+            // Image animation of BoltRobotBolt resets after 14 images
             if(imageNum < 14){
                 imageNum++;
             }
@@ -41,24 +40,29 @@ public class BoltRobotBolt extends Actor
 
     public void checkSplat()
     {
-        //After this bolt touches Paul, is atWorldEdge, or splats, then the animation will start
+        /* After BoltRobotBolt hits Paul, is at the world edge, or splats, start/continue
+           the splat animation */
         if(isTouching(Paul.class) || atWorldEdge() || splat){
-            //If the bolt just hit Paul, then splat = true and imageNum resets to 1
+            /* True only on first call of when BoltRobotBolt hit Paul or when it is at
+               world edge (start splat animation) */
             if(!splat){
                 splat = true;
-                //Does damage to Paul if it hit paul and splat at the same time
+                // True only if the bolt hit Paul, then does damage to Paul
                 if(isTouching(Paul.class)){
                     doDamage(50);
                 }
                 imageNum = 1;
             }
+            
             time++;
+            
             if(time%4 == 0){
-                //Since there are 5 different images for the bolt splat, after 5 image changes, the bolt will be removed
+                // BoltRobotBolt is removed after splat animation is complete
                 if(imageNum == 5){
                     getWorld().removeObject(this);
                 }
-                else{
+                else
+                {
                     setImage("BoltRobotBoltSplat-" + imageNum + ".png");
                     imageNum++;
                 }
@@ -66,7 +70,8 @@ public class BoltRobotBolt extends Actor
         }
     }
 
-    public void doDamage(int damage){
+    public void doDamage(int damage)
+    {
         Paul p = ((Paul)getOneIntersectingObject(Paul.class));
         //Does damage to Paul
         p.takeDamage(damage);
