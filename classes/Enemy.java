535e7dbca50eae7c52b3493632f9d96b93748475
Paul Seung Mo Lim <Paulsmlim@gmail.com>
2018-01-05 13:56:54 -0500

    Update Enemy.java

--- classes/Enemy.java
+++ classes/Enemy.java
@@ -1,19 +1,12 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;
 
 /**
- * Write a description of class Enemy here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Base class of all Enemies/Robots
  */
 public class Enemy extends Actor
 {
-    /**
-     * Act - do whatever the Enemy wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     public void act() 
     {
-        // Add your action code here.
+        
     }    
 }
