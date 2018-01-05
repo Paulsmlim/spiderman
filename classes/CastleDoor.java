d8a06e490873fb41126fd7cf3f6cc3251ed6ab23
Paul Seung Mo Lim <Paulsmlim@gmail.com>
2018-01-05 13:58:16 -0500

    Update CastleDoor.java

--- classes/CastleDoor.java
+++ classes/CastleDoor.java
@@ -1,7 +1,7 @@
 import greenfoot.*;
 
 /**
- * Door in front of Castle object that leads to RobotCastle2
+ * Door in front of Castle object that leads to RobotCastle1
  */
 public class CastleDoor extends Actor
 {
