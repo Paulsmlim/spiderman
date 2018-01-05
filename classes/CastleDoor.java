a62d4191cd4563450927b56b2f0f8e090179fdb3
Paul Seung Mo Lim <Paulsmlim@gmail.com>
2018-01-05 13:54:23 -0500

    Update CastleDoor.java

--- classes/CastleDoor.java
+++ classes/CastleDoor.java
@@ -1,19 +1,12 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;
 
 /**
- * Write a description of class CastleDoor here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Door in front of Castle object that leads to RobotCastle2
  */
 public class CastleDoor extends Actor
 {
-    /**
-     * Act - do whatever the CastleDoor wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     public void act() 
     {
        
-    }    
+    }
 }
